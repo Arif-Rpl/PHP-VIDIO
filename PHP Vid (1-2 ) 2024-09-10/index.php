9eab885a186633544245447ccbeaa4b06949e751
Arif-Rpl <mishbahulmaarif972@gmail.com>
2024-09-10 08:42:12 +0700

    Add files via upload

---
+++ PHP Vid (1-2 ) 2024-09-10/index.php
@@ -0,0 +1,25 @@
+<?php
+
+    echo "Belajar php";
+
+    echo "<br>";
+
+    echo "Saya Siswa SMK";
+
+    echo '<h1 style="background-color:cyan">belajar php itu susah</h1>
+        <p style="background-color:pink">Saya benci belajar php</p>
+    ';
+
+    echo "Saya bermain" . " Free Fire" . "<br>";
+
+    echo "tahun " . 2024;
+
+    echo "<br>";
+
+    echo 5 - 2;
+
+?>
+
+
+
+ 
