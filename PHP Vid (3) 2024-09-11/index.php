cdcbde60b1319ee21bee66c9097c290eb26bb8a5
Arif-Rpl <mishbahulmaarif972@gmail.com>
2024-09-11 08:34:58 +0700

    Add files via upload

---
+++ PHP Vid (3) 2024-09-11/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $text = "saya bermain free fire";
+
+    echo $text;
+
+    echo "<br>";
+    
+    $number = 69;
+
+    echo 'nomor ' . $number;
+    
+    echo "<br>";
+    var_dump($text);
+    echo "<br>";
+    var_dump($number+1000);
+?>
